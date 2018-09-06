fd74026c6ac7641ae761d2e00009cdfa23bcf1d4
Marsel Gray <marselgray@gmail.com>
2018-09-06 13:06:13 -0400

    added event listeners

--- app.js
+++ app.js
@@ -8,21 +8,31 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
 
-//this is a function express because the function is set to a variable
-dice = Math.floor(Math.random() * 6 ) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
-
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    //random number
+    //this is a function express because the function is set to a variable
+    dice = Math.floor(Math.random() * 6 ) + 1;
+
+    //display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    
+    //update round score IF the rolles is NOT 1
+
+})
+
