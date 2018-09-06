cc35896fdce45cdf87fc743027f89c5f6c741a4c
Marsel Gray <marselgray@gmail.com>
2018-09-06 14:25:21 -0400

    updating scores

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -33,6 +33,23 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     diceDOM.src = 'dice-' + dice + '.png';
     
     //update round score IF the rolles is NOT 1
+    if (dice > 1) {
+        //add score
+        roundScore += dice;
+        //roundScore = roundScore + dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        // next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
 
+        document.getElementById('current-0').textContent = '0'; 
+        document.getElementById('current-1').textContent = '0';    
+        
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+    }
 })
 
