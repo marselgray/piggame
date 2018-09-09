37352a435476b71d48e8209b1d9398b466dc555a
Marsel Gray <marselgray@gmail.com>
2018-09-09 15:33:33 -0400

    added new game function

--- app.js
+++ app.js
@@ -10,16 +10,32 @@ GAME RULES:
 */
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+//begins new game
+function init(){
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
 
-document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice').style.display = 'none';
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+
+}
+init();
+
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
     //random number
@@ -75,3 +91,6 @@ function nextPlayer(){
 
     document.querySelector('.dice').style.display = 'none';
 }
+
+//new game button
+document.querySelector('.btn-new').addEventListener('click', init);
